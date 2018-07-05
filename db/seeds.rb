75e358ba91b48d28571feba227748e8b638f8d7d
zhaozongchang <xingyingsuifeng@gmail.com>
2018-07-05 15:51:28 +0800

    change admin/product bakend panel

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,3 +16,13 @@ u.password_confirmation = "111111"   # 最少要六码
 
 u.is_admin = true
 u.save
+
+u = User.new
+u.email = "13188887761@163.com"           # 可以改成自己的 email
+
+u.password = "111111"                # 最少要六码
+
+u.password_confirmation = "111111"   # 最少要六码
+
+u.is_admin = true
+u.save
